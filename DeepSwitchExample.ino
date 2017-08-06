f94408cd67f1f362c9c2566317fb492e86fdb050
Lucas Thome da Silva <30710328+LukasThome@users.noreply.github.com>
2017-08-06 12:51:11 -0300

    Update DeepSwitchExample.ino

--- DeepSwitchExample.ino
+++ DeepSwitchExample.ino
@@ -1,24 +1,24 @@
 
 int led1 = 0;
 int led2 = 1;
-int chave1 = A3;
+int chave1 = A3; 			//DeepSwitch declared
 
 void setup() {
   
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
-  pinMode(chave1, INPUT);
+  pinMode(chave1, INPUT);		
 
 }
 
 void loop() {  
 digitalRead(A3);
   		
-  if (digitalRead(A3) == 0){ 			//Se a chave estiver desligada ...
+  if (digitalRead(A3) == 0){ 			//DeepSwitch off ...
 	digitalWrite(led2, LOW);	
     	digitalWrite(led1, HIGH);	
   	
-  	}else{					//Se estiver ligada ...
+  	}else{					//DeepSwitch on ...
     	      digitalWrite(led1, LOW);	
     	      digitalWrite(led2, HIGH);	
   		
